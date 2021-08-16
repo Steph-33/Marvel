cb1845e26571102ec082b7b3c800a15d932c797a
Stéphane PATERNA <paterna@club-internet.fr>
2021-08-16 10:55:47 +0200

    Mise en forme du footer

---
+++ Front-End/front-end/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Footer() {
+    return (
+        <div className="footer">
+            <img
+                src="/assets/images/logo_marvel.png"
+                alt="logo"
+            />
+        </div>
+    )
+}
--- Front-End/front-end/src/pages/Characters.js
+++ Front-End/front-end/src/pages/Characters.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Nav from '../components/Nav'
+import Footer from '../components/Footer'
 
 export default function Characters() {
     return (
         <div>
             <Nav/>
             <h2> Salut !!! </h2>
+            <Footer/>
         </div>
     )
 }
